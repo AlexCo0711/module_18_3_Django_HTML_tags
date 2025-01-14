e499abdc08ff6180ae0557338131a9bc8c245cf7
Александр <alexco@list.ru>
2025-01-14 20:04:19 +0300

    Add files via upload
    Домашнее задание по теме "Базовые HTML тэги в шаблонах".

---
+++ UrbanDjango/task3/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class Service(TemplateView):
+    template_name = 'third_task/service.html'
+
+
+def in_work(request):
+    title = 'ОБОРУДОВАНИЕ'
+    device1 = 'Принтеры 1' # наименование устройства для context
+    device2 = 'Сканеры 2' # наименование устройства для context
+    device3 = 'МФУ 3' # наименование устройства для context
+    context = {
+        'title' : title,
+        'device1' : device1, # устройство закоментировано в in_work.html вариант 1
+        'device2' : device2, # устройство закоментировано в in_work.html вариант 1
+        'device3' : device3, # устройство закоментировано в in_work.html вариант 1
+        'devices' : ['Принтеры HP', 'Сканеры HP', 'МФУ HP'] # в in_work.html вариант 2
+    }
+    return render(request, 'third_task/in_work.html', context)
+
+def ready(request):
+    return render(request, 'third_task/ready.html')
