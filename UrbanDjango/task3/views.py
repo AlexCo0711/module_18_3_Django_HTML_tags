f84a57c8ff66af996ce4ee20c7e00d6dff3eede9
Александр <alexco@list.ru>
2025-01-14 20:58:50 +0300

    Update views.py

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -11,12 +11,13 @@ def in_work(request):
     device1 = 'Принтеры 1' # наименование устройства для context
     device2 = 'Сканеры 2' # наименование устройства для context
     device3 = 'МФУ 3' # наименование устройства для context
+    devices = ['Принтеры HP', 'Сканеры HP', 'МФУ HP']
     context = {
         'title' : title,
         'device1' : device1, # устройство закоментировано в in_work.html вариант 1
         'device2' : device2, # устройство закоментировано в in_work.html вариант 1
         'device3' : device3, # устройство закоментировано в in_work.html вариант 1
-        'devices' : ['Принтеры HP', 'Сканеры HP', 'МФУ HP'], # в in_work.html вариант 2
+        'devices' : devices, # в in_work.html вариант 2
     }
     return render(request, 'third_task/in_work.html', context)
 
